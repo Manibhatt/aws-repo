87e5c23ca093aa8d65a33091403097d0c4d174a8
Mayank Bhatt <mayankbhatt404@gmail.com>
2022-10-26 01:37:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,9 +40,6 @@ resource "aws_instance" "myFirstInstance" {
   key_name = var.key_name
   instance_type = var.instance_type
   security_groups= [var.security_group]
-  tags= {
-    Name = var.tag_name
-  }
 }
 
 # Create Elastic IP address
